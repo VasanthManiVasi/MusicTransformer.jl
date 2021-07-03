154e12cbf13398c721004621f1944f98734531dc
Vasanth Mani Vasi <vasanth.mani.vasi.2@gmail.com>
2021-07-03 14:56:32 +0530

    Implement tensor2tensor positional embedding

--- src/models.jl
+++ src/models.jl
@@ -1,4 +1,4 @@
-using Flux: @functor
+using Flux: unsqueeze, @functor
 using MacroTools: @forward
 
 using Transformers.Basic
@@ -35,6 +35,17 @@ function BaselineMusicTransformer(size::Int, head::Int, hs::Int, ps::Int, layers
             Dense(size, 310),
         )
     )
+end
+
+function PositionEmbedding(size::Int, max_len::Int = 2048)
+    # Follows the tensor2tensor implementation - which is used by the unconditional 16L Music Transformer
+    num_timescales = size / 2
+    positions = Float32.(collect(0.0:max_len-1))
+    log_timescale_increment = log(1e4) / (num_timescales-1)
+    inv_timescales = Float32.(exp.(collect(0.0:(num_timescales-1)) .* -log_timescale_increment))
+    scaled_time = unsqueeze(positions, 2) * unsqueeze(inv_timescales, 1)
+    embedding = hcat(sin.(scaled_time), cos.(scaled_time))
+    Transformers.Basic.PositionEmbedding(false, collect(embedding'))
 end
 
 function (mt::BaselineMusicTransformer)(embeds::T) where T
