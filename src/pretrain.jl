9cf42e0b2813b439a6495126640a990f4324a1d6
Vasanth Mani Vasi <vasanth.mani.vasi.2@gmail.com>
2021-07-05 16:54:22 +0530

    Remove redundant checks in ckpt_to_jld2

--- src/pretrain.jl
+++ src/pretrain.jl
@@ -34,9 +34,6 @@ end
 Loads the pre-trained model weights from a tensorflow checkpoint and saves to JLD2
 """
 function ckpt_to_jld2(ckptpath::String, ckptname::String; savepath::String="./")
-    files = readdir(ckptpath)
-    ckptname ∉ files && error("The checkpoint file $ckptname is not found")
-    ckptname*".meta" ∉ files && error("The checkpoint meta file is not found")
     weights = readckpt(joinpath(ckptpath, ckptname))
     jld2name = normpath(joinpath(savepath, ckptname[1:end-5]*".jld2"))
     @info "Saving the model weights to $jld2name"
