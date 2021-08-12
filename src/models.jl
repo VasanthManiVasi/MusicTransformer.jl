336be95359ecfb8a148365db5783c299f244483d
Vasanth Mani Vasi <vasanth.mani.vasi.2@gmail.com>
2021-08-12 18:43:21 +0530

    Add MelodyConditionedMusicTransformer

--- src/models.jl
+++ src/models.jl
@@ -1,4 +1,4 @@
-export UnconditionalMusicTransformer
+export UnconditionalMusicTransformer, MelodyConditionedMusicTransformer
 
 using Flux: unsqueeze, @functor
 using MacroTools: @forward
@@ -51,6 +51,91 @@ function Base.show(io::IO, mt::UnconditionalMusicTransformer)
     print(io, "UnconditionalMusicTransformer(")
     print(io, "layers=$num_layers, ")
     print(io, "head=$(mt.ts[layer_1].mh.head), ")
+    print(io, "head_size=$(hs), ")
+    print(io, "pwffn_size=$(ps), ")
+    print(io, "size=$(h))")
+end
+
+struct MelodyConditionedMusicTransformer{EE<:Stack, E<:Stack, DE<:Stack, D<:Stack} <: MusicTransformerModel
+    encoder_embedding::EE
+    encoder::E
+    decoder_embedding::DE
+    decoder::D
+end
+
+@functor MelodyConditionedMusicTransformer
+
+@forward MelodyConditionedMusicTransformer.ts Base.getindex, Base.length
+
+function MelodyConditionedMusicTransformer(size::Int, heads::Int, ps::Int,
+                                     encoder_layers::Int, decoder_layers::Int)
+
+    rem(size, heads) != 0 && error("size not divisible by heads")
+    MelodyConditionedMusicTransformer(size, heads, div(size, heads), ps, encoder_layers, decoder_layers)
+end
+
+function MelodyConditionedMusicTransformer(size::Int, head::Int, hs::Int, ps::Int,
+                                     encoder_layers::Int, decoder_layers::Int)
+
+    MelodyConditionedMusicTransformer(
+        Stack(
+            @nntopo_str(
+                "(indices, tid):indices => e:tid => te:(e, te) => out => pe:(out, pe) => enc_input"),
+            Embed(size, 92, scale=sqrt(size)),
+            Embed(size, 32),
+            # Add target space embedding
+            (e, te) -> Float32.(e .+ te),
+            PositionEncoding(size, 4096),
+            # Add position embedding
+            (e, pe) -> (e .+ pe)
+        ),
+        Stack(
+            @nntopo_str("input => $encoder_layers => output"),
+            [
+                MusicTransformerBlock(size, head, hs, ps; future=true, pdrop=0)
+                for i = 1:encoder_layers
+            ]...,
+            LayerNorm(size),
+        ),
+        Stack(
+            @nntopo_str("indices => e => pe:(e, pe) => dec_input"),
+            Embed(size, 310, scale=sqrt(size)),
+            PositionEncoding(size, 4096),
+            # Add position embedding
+            (e, pe) -> (e .+ pe)
+        ),
+        Stack(
+            @nntopo_str(
+                "((input, m) => input:(input, m)) => $decoder_layers:input => norm => output"),
+            [
+                MusicTransformerDecoder(size, head, hs, ps; pdrop=0)
+                for i = 1:decoder_layers
+            ]...,
+            LayerNorm(size),
+            Dense(size, 310), # softmax embedding layer
+        )
+    )
+end
+
+function (mt::MelodyConditionedMusicTransformer)(inputs::Vector{Int}, targets::Vector{Int})
+    target_space_id = [1]
+    encoder_inputs = mt.encoder_embedding(inputs, target_space_id)
+    encoder_context = mt.encoder(encoder_inputs)
+    decoder_inputs = mt.decoder_embedding(targets)
+    logits = mt.decoder(decoder_inputs, encoder_context)
+
+    logits, encoder_context
+end
+
+function Base.show(io::IO, mt::MelodyConditionedMusicTransformer)
+    hs = div(size(mt.encoder[1].mh.iqproj.W)[1], mt.encoder[1].mh.head)
+    h, ps = size(mt.encoder[1].pw.dout.W)
+    encoder_layers = length(mt.encoder) - 1 # Ignore postprocess layernorm
+    decoder_layers = length(mt.decoder) - 2 # Ignore postprocess layernorm and softmax embedding layer
+    print(io, "MelodyConditionedMusicTransformer(")
+    print(io, "encoder_layers=$encoder_layers, ")
+    print(io, "decoder_layers=$decoder_layers, ")
+    print(io, "head=$(mt.encoder[1].mh.head), ")
     print(io, "head_size=$(hs), ")
     print(io, "pwffn_size=$(ps), ")
     print(io, "size=$(h))")
